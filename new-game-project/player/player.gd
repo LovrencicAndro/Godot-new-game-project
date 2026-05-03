d7634efe6c5857b34529b21fc483943da8c3dedd
LovrencicAndro <lovrencicandro7@gmail.com>
2026-05-03 20:02:11 +0200

    init

---
+++ new-game-project/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+func _ready():   #  when the game starts
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * 1
+		%Camera3D.rotation_degrees.x -= event.relative.y * 0.5
+		#%Camera3D.rotation_degrees.x = clamp(
+		#	%Camera3D.rotation_degrees.x, -80.0, 50
+		#)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
